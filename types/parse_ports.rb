8e0b75c48572005deb8b2e59d61fabd3b1a0948b
Stefan Karpinski <stefan.karpinski@gmail.com>
2008-11-07 12:41:49 -0800

    type/parse_ports.rb: type_map => map; remove "emit".

--- types/parse_ports.rb
+++ types/parse_ports.rb
@@ -1,6 +1,6 @@
 #!/usr/bin/env ruby
 
-$type_map = Hash.new{|h,k| h[k] = Hash.new{|h,k| h[k] = []}}
+$map = Hash.new{|h,k| h[k] = Hash.new{|h,k| h[k] = []}}
 
 proto = { "tcp" => 6, "udp" => 17 }
 while line = gets
@@ -9,16 +9,16 @@ while line = gets
     desc = $1.downcase
     port = Integer($2)
     prot = proto[$3]
-    $type_map[port][prot] << desc unless
-    $type_map[port][prot].include?(desc)
+    $map[port][prot] << desc unless
+    $map[port][prot].include?(desc)
   end
 end
 
 def override port, name, proto=nil
   if proto
-    $type_map[port][proto] = name
+    $map[port][proto] = name
   else
-    $type_map[port][6] = $type_map[port][17] = name
+    $map[port][6] = $map[port][17] = name
   end
 end
 
@@ -26,25 +26,16 @@ override 0,  "spr-itunes", 6
 override 42, "nameserver"
 override 80, "http"
 
-def emit port
-  if $type_map[port][6] == $type_map[port][17]
-    puts "#{port},*,#{$type_map[port][6]}"
-  else
-    puts "#{port},6,#{$type_map[port][6]}"   unless $type_map[port][6].empty?
-    puts "#{port},17,#{$type_map[port][17]}" unless $type_map[port][17].empty?
-  end
-end
-
 $, = '/'
-$type_map.keys.sort.each do |port|
-  if $type_map[port][6] != $type_map[port][17]
-    $type_map[port][6].each{|x| x.sub!(/-?tcp(-\d+)?$/,'\1')}  unless $type_map[port][17].empty?
-    $type_map[port][17].each{|x| x.sub!(/-?udp(-\d+)?$/,'\1')} unless $type_map[port][6].empty?
+$map.keys.sort.each do |port|
+  if $map[port][6] != $map[port][17]
+    $map[port][6].each{|x| x.sub!(/-?tcp(-\d+)?$/,'\1')}  unless $map[port][17].empty?
+    $map[port][17].each{|x| x.sub!(/-?udp(-\d+)?$/,'\1')} unless $map[port][6].empty?
   end
-  if $type_map[port][6] == $type_map[port][17]
-    puts "#{port},*,#{$type_map[port][6]}"
+  if $map[port][6] == $map[port][17]
+    puts "#{port},*,#{$map[port][6]}"
   else
-    puts "#{port},6,#{$type_map[port][6]}"   unless $type_map[port][6].empty?
-    puts "#{port},17,#{$type_map[port][17]}" unless $type_map[port][17].empty?
+    puts "#{port},6,#{$map[port][6]}"   unless $map[port][6].empty?
+    puts "#{port},17,#{$map[port][17]}" unless $map[port][17].empty?
   end
 end
