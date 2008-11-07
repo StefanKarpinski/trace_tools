3c377be6ac7021df1de48f5a98a77e51f23954e8
Stefan Karpinski <stefan.karpinski@gmail.com>
2008-11-07 14:18:36 -0800

    unpack: added -P <prefix> option to add initial field to output.

--- src/unpack.c
+++ src/unpack.c
@@ -19,12 +19,13 @@ int main(int argc, char ** argv) {
   // option variables
   u_int8_t  input  = INPUT_UNKNOWN;
   u_int8_t  output = OUTPUT_TAB;
+  char     *prefix = NULL;
   char     *format = NULL;
   u_int32_t offset = 0;
 
   // parse options, leave arguments
   int i;
-  while ((i = getopt(argc,argv,"fptcF:o:")) != -1) {
+  while ((i = getopt(argc,argv,"fptcP:F:o:")) != -1) {
     switch (i) {
 
       case 'f':
@@ -41,6 +42,9 @@ int main(int argc, char ** argv) {
       case 'c':
         output = OUTPUT_CSV;
         format = NULL;
+        break;
+      case 'P':
+        prefix = optarg;
         break;
       case 'F':
         format = optarg;
@@ -61,6 +65,16 @@ int main(int argc, char ** argv) {
   }
   if (input == INPUT_UNKNOWN)
     die("Please specify input type: -f for flows or -p for packets.\n");
+  if (prefix && !format) {
+    char *p = prefix;
+    int len = strlen(p);
+    prefix = malloc(len+2);
+    strcpy(prefix,p);
+    prefix[len] =
+      output == OUTPUT_TAB ? '\t' :
+      output == OUTPUT_CSV ? ','  : '\0';
+    prefix[len+1] = '\0';
+  }
   if (format) {
     format = strdup(format);
     c_unescape(format);
@@ -79,8 +93,8 @@ int main(int argc, char ** argv) {
           inet_ntop(AF_INET,&flow.src_ip,src,sizeof(src));
           inet_ntop(AF_INET,&flow.dst_ip,dst,sizeof(dst));
           if (!format) format =
-            output == OUTPUT_TAB ? "%u\t%u\t%s\t%s\t%u\t%u\t%s\t%s\n" :
-            output == OUTPUT_CSV ? "%u,%u,%s,%s,%u,%u,%s,%s\n" : NULL;
+            output == OUTPUT_TAB ? "%s%u\t%u\t%s\t%s\t%u\t%u\t%s\t%s\n" :
+            output == OUTPUT_CSV ? "%s%u,%u,%s,%s,%u,%u,%s,%s\n" : NULL;
           char *proto_str = proto_name(flow.proto);
           char *desc = NULL;
           switch (flow.proto) {
@@ -95,6 +109,7 @@ int main(int argc, char ** argv) {
               break;
           }
           printf(format,
+            prefix ? prefix : "",
             offset+index++,
             flow.proto,
             src, dst,
@@ -118,9 +133,14 @@ int main(int argc, char ** argv) {
           ntoh_packet(&packet);
           double time = packet.sec + packet.usec*1e-6;
           if (!format) format =
-            output == OUTPUT_TAB ? "%u\t%17.6f\t%u\n" :
-            output == OUTPUT_CSV ? "%u,%.6f,%u\n" : NULL;
-          printf(format,offset+packet.flow,time,packet.size);
+            output == OUTPUT_TAB ? "%s%u\t%17.6f\t%u\n" :
+            output == OUTPUT_CSV ? "%s%u,%.6f,%u\n" : NULL;
+          printf(format,
+            prefix ? prefix : "",
+            offset+packet.flow,
+            time,
+            packet.size
+          );
         }
         fclose(file);
         wait(NULL);
