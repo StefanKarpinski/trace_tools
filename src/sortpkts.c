1aa32e483884645e74572c4e3c90a4d3afb625f0
Stefan Karpinski <stefan.karpinski@gmail.com>
2008-11-10 16:38:07 -0800

    sortpkts: new syntax for specifying sort orders.
    Instead of using different flags for major and minor orders,
    now the order of the flags implies whether they are major or
    minor: the major field is specified first, followed by minor.
    
    Also, if the minor field is not specified, it is chosen to
    jive with the major field. Before it could clash, causing an
    error unless both major and minor were given.

--- src/sortpkts.c
+++ src/sortpkts.c
@@ -33,29 +33,36 @@ declare_sorter(lt_time_size,htonl,sec ,htonl,usec,htons,size,htonl,flow)
 declare_sorter(lt_size_flow,htons,size,htonl,flow,htonl,sec ,htonl,usec)
 declare_sorter(lt_size_time,htons,size,htonl,sec ,htonl,usec,htonl,flow)
 
-#define SORT_FLOW 0
-#define SORT_TIME 1
-#define SORT_SIZE 2
+#define SORT_NONE 0
+#define SORT_FLOW 1
+#define SORT_TIME 2
+#define SORT_SIZE 3
 
-#define SORT_ORDER(major,minor) (3*major+minor)
+#define SORT_ORDER(major,minor) ((SORT_SIZE+1)*major+minor)
+
+#define SET_SORT(m,f) \
+  switch (m) { \
+    case 1: major = f; break; \
+    case 0: minor = f; break; \
+    default: \
+      die("You can only specify two sort fields.\n"); \
+  }
 
 int main(int argc, char ** argv) {
 
+  int m = 1;
   int major = SORT_FLOW;
-  int minor = SORT_TIME;
+  int minor = SORT_NONE;
   int parallel = 0;
 
   int i;
-  while ((i = getopt(argc,argv,"FTSftsp")) != -1) {
+  while ((i = getopt(argc,argv,"ftsp")) != -1) {
     switch (i) {
-      case 'F': major = SORT_FLOW; break;
-      case 'T': major = SORT_TIME; break;
-      case 'S': major = SORT_SIZE; break;
 
-      case 'f': minor = SORT_FLOW; break;
-      case 't': minor = SORT_TIME; break;
-      case 's': minor = SORT_SIZE; break;
-      
+      case 'f': SET_SORT(m--,SORT_FLOW); break;
+      case 't': SET_SORT(m--,SORT_TIME); break;
+      case 's': SET_SORT(m--,SORT_SIZE); break;
+
       case 'p': parallel = 1; break;
 
       case '?':
@@ -69,21 +76,42 @@ int main(int argc, char ** argv) {
   }
   if (major == minor)
     die("Major and minor sort fields must differ.\n");
+  if (minor == SORT_NONE)
+    minor = (major != SORT_FLOW) ? SORT_FLOW : SORT_TIME;
 
+  char *desc;
   int (*lt)(void *m, size_t a, size_t b);
   switch (SORT_ORDER(major,minor)) {
-    case SORT_ORDER(SORT_FLOW,SORT_TIME): lt = lt_flow_time; break;
-    case SORT_ORDER(SORT_FLOW,SORT_SIZE): lt = lt_flow_size; break;
-    case SORT_ORDER(SORT_TIME,SORT_FLOW): lt = lt_time_flow; break;
-    case SORT_ORDER(SORT_TIME,SORT_SIZE): lt = lt_time_size; break;
-    case SORT_ORDER(SORT_SIZE,SORT_FLOW): lt = lt_size_flow; break;
-    case SORT_ORDER(SORT_SIZE,SORT_TIME): lt = lt_size_time; break;
+    case SORT_ORDER(SORT_FLOW,SORT_TIME):
+      desc = "flow, time then size";
+      lt = lt_flow_time;
+      break;
+    case SORT_ORDER(SORT_FLOW,SORT_SIZE):
+      desc = "flow, size then time";
+      lt = lt_flow_size;
+      break;
+    case SORT_ORDER(SORT_TIME,SORT_FLOW):
+      desc = "time, flow then size";
+      lt = lt_time_flow;
+      break;
+    case SORT_ORDER(SORT_TIME,SORT_SIZE):
+      desc = "time, size then flow";
+      lt = lt_time_size;
+      break;
+    case SORT_ORDER(SORT_SIZE,SORT_FLOW):
+      desc = "size, flow then time";
+      lt = lt_size_flow;
+      break;
+    case SORT_ORDER(SORT_SIZE,SORT_TIME):
+      desc = "size, time then flow";
+      lt = lt_size_time;
+      break;
   }
 
   if (optind == argc) argc++;
   for (i = optind; i < argc; i++) {
     if (optind != argc)
-      fprintf(stderr,"sorting %s...\n",argv[i]);
+      fprintf(stderr,"sorting %s by %s...\n",argv[i],desc);
     if (parallel && fork()) continue;
 
     FILE *file = fopen(argv[i],"r+");
