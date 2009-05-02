ac771c47cfd3d5e27f5e43b3e9a857a76500b057
Stefan Karpinski <stefan.karpinski@gmail.com>
2009-05-02 16:41:43 -0700

    stats: implement flow index output, reindexing, offset adjustment.

--- src/stats.c
+++ src/stats.c
@@ -13,6 +13,10 @@ const char *usage =
   "\n"
   "  -m <integer>   Only output flows with this many packets.\n"
   "\n"
+  "  -I             Print flow indices.\n"
+  "  -R             Reindex flows (imples -I).\n"
+  "  -o <integer>   Flow index offset (default: 0).\n"
+  "\n"
   "  -c             CSV output.\n"
   "  -t             Tab-delimited output.\n"
   "  -d <string>    Custom-delimited output.\n"
@@ -25,6 +29,10 @@ int size_ps_max = 2;
 int ival_ps_max = 2;
 
 int min_packets = 1;
+
+int indices = 0;
+int reindex = 0;
+int offset  = 0;
 
 char *const comma = ",";
 char *const tab = "\t";
@@ -39,6 +47,9 @@ void parse_opts(int argc, char **argv) {
     { "sizes",       required_argument, 0, 'Z' },
     { "intervals",   required_argument, 0, 'V' },
     { "min-packets", required_argument, 0, 'm' },
+    { "indices",     no_argument,       0, 'I' },
+    { "reindex",     no_argument,       0, 'R' },
+    { "offset",      required_argument, 0, 'o' },
     { "csv",         no_argument,       0, 'c' },
     { "tab",         no_argument,       0, 't' },
     { "delimiter",   required_argument, 0, 'd' },
@@ -47,7 +58,7 @@ void parse_opts(int argc, char **argv) {
   };
 
   int c;
-  while ((c = getopt_long(argc,argv,"Z::V::m:ctd:h",longopts,0)) != -1) {
+  while ((c = getopt_long(argc,argv,"Z::V::m:IRo:ctd:h",longopts,0)) != -1) {
     switch (c) {
       case 'Z':
         size_ps_max = atoi(optarg);
@@ -58,6 +69,17 @@ void parse_opts(int argc, char **argv) {
 
       case 'm':
         min_packets = atoi(optarg);
+        break;
+
+      case 'I':
+        indices = 1;
+        break;
+      case 'R':
+        indices = 1;
+        reindex = 1;
+        break;
+      case 'o':
+        offset = atoi(optarg);
         break;
 
       case 'c':
@@ -90,21 +112,19 @@ double packet_time;
 uint last_flow = 0xffff;
 double last_time = -INFINITY;
 
-long long packets;
+long long packets, flow;
 unsigned long long *size_ps;
 long double *ival_ps;
-
-#define power(x,y) (y==1 ? x : pow(x,y))
 
 void update() {
   int i;
   packets++;
   for (i = 0; i < size_ps_max; i++)
-    size_ps[i] += power(packet.size,i+1);
+    size_ps[i] += pow(packet.size,i+1);
   if (packet.flow != last_flow) return;
   double interval = packet_time - last_time;
   for (i = 0; i < ival_ps_max; i++)
-    ival_ps[i] += power(interval,i+1);
+    ival_ps[i] += pow(interval,i+1);
 }
 
 #define delim(more) (more ? delimiter : "")
@@ -113,6 +133,8 @@ void flush() {
   if (size_ps && ival_ps) {
     if (packets >= min_packets) {
       int i;
+      if (indices)
+        printf("%lld%s",offset + (reindex ? flow++ : packet.flow),delim(1));
       printf("%llu%s",packets,delim(size_ps_max || ival_ps_max));
       for (i = 0; i < size_ps_max; i++)
         printf("%llu%s",size_ps[i],delim(i+1 < size_ps_max || ival_ps_max));
