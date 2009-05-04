018213ea9ce9f5f9b9b1687c92b6740d8f957877
Stefan Karpinski <stefan.karpinski@gmail.com>
2009-05-03 21:57:19 -0700

    histogram: added usage message and base offset option.

--- src/histogram.c
+++ src/histogram.c
@@ -1,30 +1,50 @@
 const char *usage =
   "Usage:\n"
+  "  histogram -n<cols> [-i<inc>] <data files>\n"
+  "\n"
+  "  Take row-oriented integer data and construct histograms\n"
+  "  out of it the values observed.\n"
+  "\n"
+  "Options:\n"
+  "  -n <integer>   The number of columns.\n"
+  "  -o <integer>   Initial offset (default: 0).\n"
+  "  -i <integer>   Offset increment (default: 0).\n"
+  "\n"
+  "Notes:\n"
+  "  The number of columns is the maximum output column value.\n"
+  "  If larger values than this occur, they will be reduced\n"
+  "  modulo this number. The offset value is incremented by\n"
+  "  this amount for each value seen in a row.\n"
   "\n"
 ;
 
 #include "common.h"
 
 int n = 0;
+int offset = 0;
 int inc = 0;
 
 void parse_opts(int argc, char **argv) {
 
   static struct option longopts[] = {
     { "columns",   required_argument, 0, 'n' },
+    { "offset",    required_argument, 0, 'o' },
     { "increment", required_argument, 0, 'i' },
     { "help",      no_argument,       0, 'h' },
     { 0, 0, 0, 0 }
   };
 
   int c;
-  while ((c = getopt_long(argc,argv,"n:i:h",longopts,0)) != -1) {
+  while ((c = getopt_long(argc,argv,"n:o:i:h",longopts,0)) != -1) {
     switch (c) {
 
       case 'n':
         n = atoi(optarg);
         if (n <= 0)
           die("Column number must be positive.\n");
+        break;
+      case 'o':
+        offset = atoi(optarg);
         break;
       case 'i':
         inc = atoi(optarg);
@@ -60,7 +80,7 @@ int main(int argc, char **argv) {
     char *line;
     size_t length;
     while (line = fgetln(file,&length)) {
-      int offset = 0;
+      int o = offset;
       for (;;) {
         line += strcspn(line,"+-0123456789\n");
         if (*line == '\n' || *line == '\0') {
@@ -72,8 +92,8 @@ int main(int argc, char **argv) {
           break;
         }
         long long c = strtoll(line,&line,10);
-        hist[(offset + c) % n]++;
-        offset += inc;
+        hist[(o + c) % n]++;
+        o += inc;
       }
       r++;
     }
