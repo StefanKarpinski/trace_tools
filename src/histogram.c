f209453d6ef1b8c50615f52069a720be16a5d8ed
Stefan Karpinski <stefan.karpinski@gmail.com>
2009-05-30 23:40:37 -0700

    histogram: modulo index mapping and truncation; default is error.
    Previously, the default behavior was a mix of modulo mapping for
    values that were too large and just ignoring values that were too
    small--with the possibility of memory corruption (negative index
    into an array).
    
    Now the default is to error out for values not in [0,n-1]. If the
    option to use modulo mapping is used, values are mapped using the
    mod operation (this is tricky because C's % operator maps negative
    numbers to negative numbers). If the option to use truncation is
    given, then values that are too small and too large are mapped to
    0 and n-1, respectively.

--- src/histogram.c
+++ src/histogram.c
@@ -3,29 +3,30 @@ const char *usage =
   "  histogram -n<cols> [-i<inc>] <data files>\n"
   "\n"
   "  Take row-oriented integer data and construct histograms\n"
-  "  out of it the values observed.\n"
+  "  out of the values observed.\n"
   "\n"
   "Options:\n"
   "  -n <integer>   The number of columns.\n"
   "  -o <integer>   Offset to subtract (default: 1).\n"
   "  -i <integer>   Per-item increment (default: 0).\n"
   "  -x <integer>   Set <n> to <x> times the <i> value.\n"
+  "  -m             Map values to [0,n-1] by modulo operation.\n"
+  "  -t             Map values to [0,n-1] by truncation.\n"
   "  -D             Print dimensions in last row (zero value).\n"
-  "\n"
-  "Notes:\n"
-  "  The number of columns is the maximum output column value.\n"
-  "  If larger values than this occur, they will be reduced\n"
-  "  modulo this number. The offset value is incremented by\n"
-  "  this amount for each value seen in a row.\n"
   "\n"
 ;
 
 #include "common.h"
 
+#define REDUCE_ERROR    0
+#define REDUCE_MODULO   1
+#define REDUCE_TRUNCATE 2
+
 int n = 0;
 int offset = 1;
 int inc = 0;
 int print_dims = 0;
+int reduce = REDUCE_ERROR;
 
 void parse_opts(int argc, char **argv) {
 
@@ -33,14 +34,16 @@ void parse_opts(int argc, char **argv) {
     { "columns",    required_argument, 0, 'n' },
     { "offset",     required_argument, 0, 'o' },
     { "increment",  required_argument, 0, 'i' },
-    { "multiple",   required_argument, 0, 'x' },
+    { "iterate",    required_argument, 0, 'x' },
+    { "modulo",     no_argument,       0, 'm' },
+    { "truncate",   no_argument,       0, 't' },
     { "dimensions", no_argument,       0, 'D' },
     { "help",       no_argument,       0, 'h' },
     { 0, 0, 0, 0 }
   };
 
   int c;
-  while ((c = getopt_long(argc,argv,"n:o:i:x:Dh",longopts,0)) != -1) {
+  while ((c = getopt_long(argc,argv,"n:o:i:x:mtDh",longopts,0)) != -1) {
     switch (c) {
 
       case 'n':
@@ -56,6 +59,12 @@ void parse_opts(int argc, char **argv) {
         break;
       case 'x':
         n = inc * atoi(optarg);
+        break;
+      case 'm':
+        reduce = REDUCE_MODULO;
+        break;
+      case 't':
+        reduce = REDUCE_TRUNCATE;
         break;
       case 'D':
         print_dims = 1;
@@ -103,7 +112,33 @@ int main(int argc, char **argv) {
           break;
         }
         long long c = strtoll(line,&line,10);
-        hist[(c + j) % n]++;
+        long long k = c + j;
+        if (k < 0) {
+          switch (reduce) {
+            case REDUCE_MODULO:
+              k = n - (-k % n);
+              if (k == n) k = 0;
+              break;
+            case REDUCE_TRUNCATE:
+              k = 0;
+              break;
+            default:
+              die("Value too small: %lld = %lld + %lld.\n",k,c,j);
+          }
+        }
+        else if (n <= k) {
+          switch (reduce) {
+            case REDUCE_MODULO:
+              k = k % n;
+              break;
+            case REDUCE_TRUNCATE:
+              k = n - 1;
+              break;
+            default:
+              die("Value too large: %lld = %lld + %lld.\n",k,c,j);
+          }
+        }
+        hist[k]++;
         j += inc;
       }
       r++;
