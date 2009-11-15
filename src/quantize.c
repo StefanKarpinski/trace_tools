a334871f5d726b8d7254ecf32254a9ad1845dcc4
Stefan Karpinski <stefan.karpinski@gmail.com>
2009-11-15 18:29:59 -0500

    quantize: implement dequantization & "fuzzing".
    Fuzzing is quantization followed by dequantization, thus giving
    back values that are equivalent under the quantization function.
    
    Dequantization should, in general, assume that values between
    the quantized values are uniformly distributed and transform
    them back that way.

--- src/quantize.c
+++ src/quantize.c
@@ -29,28 +29,44 @@ int offset = 1;
 double power = 1;
 double base = 10;
 
+#define TRANS_QUANTIZE   0
+#define TRANS_DEQUANTIZE 1
+#define TRANS_FUZZ       2
+
+int transform = TRANS_QUANTIZE;
+
 int (*quantize)(double) = NULL;
+double (*dequantize)(int) = NULL;
 
 int quantize_floor(double);
 int quantize_power(double);
 int quantize_steplog(double);
 
+double dequantize_floor(int);
+double dequantize_power(int);
+double dequantize_steplog(int);
+
+unsigned seed = 0;
+
 void parse_opts(int argc, char **argv) {
 
   static struct option longopts[] = {
-    { "bins",    required_argument, 0, 'n' },
-    { "min",     required_argument, 0, 'm' },
-    { "max",     required_argument, 0, 'M' },
-    { "power",   required_argument, 0, 'p' },
-    { "log",     no_argument,       0, 'l' },
-    { "steplog", optional_argument, 0, 'L' },
-    { "offset",  required_argument, 0, 'o' },
-    { "help",    no_argument,       0, 'h' },
+    { "bins",       required_argument, 0, 'n' },
+    { "min",        required_argument, 0, 'm' },
+    { "max",        required_argument, 0, 'M' },
+    { "power",      required_argument, 0, 'p' },
+    { "log",        no_argument,       0, 'l' },
+    { "steplog",    optional_argument, 0, 'L' },
+    { "offset",     required_argument, 0, 'o' },
+    { "dequantize", no_argument,       0, 'd' },
+    { "fuzz",       no_argument,       0, 'f' },
+    { "seed",       required_argument, 0, 's' },
+    { "help",       no_argument,       0, 'h' },
     { 0, 0, 0, 0 }
   };
 
   int c;
-  while ((c = getopt_long(argc,argv,"n:m:M:p:lL::o:h",longopts,0)) != -1) {
+  while ((c = getopt_long(argc,argv,"n:m:M:p:lL::o:dfs:h",longopts,0)) != -1) {
     switch (c) {
 
       case 'n':
@@ -66,6 +82,7 @@ void parse_opts(int argc, char **argv) {
         break;
       case 'p':
         quantize = quantize_power;
+        dequantize = dequantize_power;
         power = atof(optarg);
         break;
       case 'l':
@@ -73,11 +90,22 @@ void parse_opts(int argc, char **argv) {
         break;
       case 'L':
         quantize = quantize_steplog;
+        dequantize = dequantize_steplog;
         if (optarg)
           base = atof(optarg);
         break;
       case 'o':
         offset = atoi(optarg);
+        break;
+
+      case 'd':
+        transform = TRANS_DEQUANTIZE;
+        break;
+      case 'f':
+        transform = TRANS_FUZZ;
+        break;
+      case 's':
+        seed = atol(optarg);
         break;
 
       case 'h':
@@ -95,9 +123,15 @@ void parse_opts(int argc, char **argv) {
     }
   }
 
-  if (!quantize)
-    quantize = n > 0 ? quantize_power : quantize_floor;
-
+  if (!quantize) {
+    if (n > 0) {
+      quantize = quantize_power;
+      dequantize = dequantize_power;
+    } else {
+      quantize = quantize_floor;
+      dequantize = dequantize_floor;
+    }
+  }
   if (quantize != quantize_power) return;
 
   if (!n)
@@ -113,10 +147,20 @@ void parse_opts(int argc, char **argv) {
     min = log(min);
     max = log(max);
   }
+
+  if (!seed) {
+    srandomdev();
+    seed = random();
+  }
+  srand48(seed);
 }
 
 int quantize_floor(double v) {
   return floor(v);
+}
+
+double dequantize_floor(int q) {
+  return ((double) q) + drand48();
 }
 
 int quantize_power(double v) {
@@ -127,6 +171,15 @@ int quantize_power(double v) {
   return q;
 }
 
+double dequantize_power(int q) {
+  if (q >= n) q = n-1;
+  if (q < 0) q = 0;
+  double d = ((double) q) + drand48();
+  double v = min+pow(d/n,1/power)*(max-min);
+  if (log_transform) v = exp(v);
+  return v;
+}
+
 int quantize_steplog(double v) {
   int m = floor(log(v)/log(base));
   int d = floor(v/pow(base,m));
@@ -134,10 +187,17 @@ int quantize_steplog(double v) {
   return q;
 }
 
+double dequantize_steplog(int q) {
+  die("Steplog dequantization not implemented.\n");
+}
+
 int main(int argc, char **argv) {
   int i;
   parse_opts(argc,argv);
   if (optind == argc) argc++;
+  char *numerics = transform == TRANS_DEQUANTIZE ?
+    "+-0123456789\n" : "+-0123456789.\n";
+
   for (i = optind; i < argc; i++) {
     FILE *file = open_arg(argv[i]);
     char *line, *buffer = NULL;
@@ -150,9 +210,29 @@ int main(int argc, char **argv) {
           if (*line) putchar('\n');
           break;
         }
-        double v = strtod(line,&line);
-        int q = quantize(v);
-        printf("%d",offset+q);
+        switch (transform) {
+          case TRANS_QUANTIZE: {
+            double v = strtod(line,&line);
+            int q = quantize(v);
+            printf("%d",q+offset);
+            break;
+          }
+          case TRANS_DEQUANTIZE: {
+            int q = strtol(line,&line,10);
+            double v = dequantize(q-offset);
+            printf("%f",v);
+            break;
+          }
+          case TRANS_FUZZ: {
+            double v = strtod(line,&line);
+            int q = quantize(v);
+            double w = dequantize(q);
+            printf("%f",w);
+            break;
+          }
+          default:
+            die("ERROR: Invalid transform badness.\n");
+        }
       }
     }
     fclose(file);
